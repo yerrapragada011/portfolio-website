9e207dc7bdb02fa71c5857eaf3f870f75ccf08fd
Agasthya Yerrapragada <yerrapragada.agasthya@gmail.com>
2025-02-20 08:03:07 -0500

    Remove screenshot api and add project pictures

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useMemo } from 'react'
+import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faReact, faNodeJs } from '@fortawesome/free-brands-svg-icons'
 import { faDatabase } from '@fortawesome/free-solid-svg-icons'
@@ -6,94 +6,63 @@ import { faHtml5, faCss3Alt, faJs } from '@fortawesome/free-brands-svg-icons'
 import { faGithub } from '@fortawesome/free-brands-svg-icons'
 import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons'
 import './Projects.css'
-// require('dotenv').config()
 
 function Projects() {
-  const [projects, setProjects] = useState([])
-
-  const projectList = useMemo(
-    () => [
-      {
-        title: 'Social Media App',
-        description:
-          'An application that allows users to register, login, or login with GitHub. Includes all CRUD operations for posts, comments, followers, and likes.',
-        demoLink: 'https://social-media-six-kappa.vercel.app/',
-        githubLink: 'https://github.com/yerrapragada011/social-media.git',
-        techStack: [faReact, faNodeJs, faDatabase],
-      },
-      {
-        title: 'Blog API',
-        description:
-          'An application that listens to two separate frontends. A reader frontend that allows you to register, login, view blog posts, and add comments. And an author frontend that allows CRUD operations for posts.',
-        demoLink: 'https://blog-api-yr9v.vercel.app/',
-        githubLink: 'https://github.com/yerrapragada011/blog-api.git',
-        techStack: [faReact, faNodeJs, faDatabase],
-      },
-      {
-        title: 'Messaging App',
-        description:
-          'An application that allows users to register, login, and view messages. Includes operations to send, receive, and delete messages.',
-        demoLink: 'https://messaging-app-mauve.vercel.app/',
-        githubLink: 'https://github.com/yerrapragada011/messaging-app.git',
-        techStack: [faReact, faNodeJs, faDatabase],
-      },
-      {
-        title: `Where's Waldo?`,
-        description:
-          'A game application that allows users to select an area on a picture to find Waldo and his friends. Users who find all characters and beat the game get added to the leaderboard.',
-        demoLink: 'https://wheres-waldo-mu.vercel.app/',
-        githubLink: 'https://github.com/yerrapragada011/wheres-waldo.git',
-        techStack: [faReact, faNodeJs, faDatabase],
-      },
-      {
-        title: 'Battleship',
-        description: `A game application that allows users to play against a computer. Whoever sinks all the other user's ships first wins!`,
-        demoLink: 'https://yerrapragada011.github.io/battleship/',
-        githubLink: 'https://github.com/yerrapragada011/battleship.git',
-        techStack: [faHtml5, faCss3Alt, faJs],
-      },
-      {
-        title: 'Todo List',
-        description:
-          'An application that allows users to create todos, create projects, edit todos, delete todos, mark todos as completed, and clear completed todos.',
-        demoLink: 'https://yerrapragada011.github.io/todo-list/',
-        githubLink: 'https://github.com/yerrapragada011/todo-list.git',
-        techStack: [faHtml5, faCss3Alt, faJs],
-      },
-    ],
-    []
-  )
-
-  const fetchScreenshot = async (url) => {
-    const accessKey = process.env.REACT_APP_SCREENSHOT_ACCESS_KEY
-
-    const apiUrl = `https://api.screenshotone.com/take?url=${encodeURIComponent(
-      url
-    )}&access_key=${accessKey}`
-
-    try {
-      const response = await fetch(apiUrl)
-      const blob = await response.blob()
-      return URL.createObjectURL(blob)
-    } catch (error) {
-      console.error('Error fetching screenshot:', error)
-      return 'default-screenshot-url.png'
-    }
-  }
-
-  useEffect(() => {
-    const fetchScreenshots = async () => {
-      const updatedProjects = await Promise.all(
-        projectList.map(async (project) => {
-          const image = await fetchScreenshot(project.demoLink)
-          return { ...project, image }
-        })
-      )
-      setProjects(updatedProjects)
-    }
-
-    fetchScreenshots()
-  }, [projectList])
+  const projects = [
+    {
+      title: 'Social Media App',
+      description:
+        'An application that allows users to register, login, or login with GitHub. Includes all CRUD operations for posts, comments, followers, and likes.',
+      demoLink: 'https://social-media-six-kappa.vercel.app/',
+      githubLink: 'https://github.com/yerrapragada011/social-media.git',
+      techStack: [faReact, faNodeJs, faDatabase],
+      image: '/images/social_media.png',
+    },
+    {
+      title: 'Blog API',
+      description:
+        'An application that listens to two separate frontends. A reader frontend that allows you to register, login, view blog posts, and add comments. And an author frontend that allows CRUD operations for posts.',
+      demoLink: 'https://blog-api-yr9v.vercel.app/',
+      githubLink: 'https://github.com/yerrapragada011/blog-api.git',
+      techStack: [faReact, faNodeJs, faDatabase],
+      image: '/images/blog_api.png',
+    },
+    {
+      title: 'Messaging App',
+      description:
+        'An application that allows users to register, login, and view messages. Includes operations to send, receive, and delete messages.',
+      demoLink: 'https://messaging-app-mauve.vercel.app/',
+      githubLink: 'https://github.com/yerrapragada011/messaging-app.git',
+      techStack: [faReact, faNodeJs, faDatabase],
+      image: '/images/messaging_app.png',
+    },
+    {
+      title: `Where's Waldo?`,
+      description:
+        'A game application that allows users to select an area on a picture to find Waldo and his friends. Users who find all characters and beat the game get added to the leaderboard.',
+      demoLink: 'https://wheres-waldo-mu.vercel.app/',
+      githubLink: 'https://github.com/yerrapragada011/wheres-waldo.git',
+      techStack: [faReact, faNodeJs, faDatabase],
+      image: '/images/wheres_waldo.png',
+    },
+    {
+      title: 'Battleship',
+      description: `A game application that allows users to play against a computer. Whoever sinks all the other user's ships first wins!`,
+      demoLink: 'https://yerrapragada011.github.io/battleship/',
+      githubLink: 'https://github.com/yerrapragada011/battleship.git',
+      techStack: [faHtml5, faCss3Alt, faJs],
+      image: '/images/battleship.png',
+    },
+    {
+      title: 'Todo List',
+      description:
+        'An application that allows users to create todos, create projects, edit todos, delete todos, mark todos as completed, and clear completed todos.',
+      demoLink: 'https://yerrapragada011.github.io/todo-list/',
+      githubLink: 'https://github.com/yerrapragada011/todo-list.git',
+      techStack: [faHtml5, faCss3Alt, faJs],
+      image: '/images/todo_list.png',
+    },
+  ]
 
   return (
     <section id="projects" className="projects roboto-regular">
